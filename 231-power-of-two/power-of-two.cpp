54950d2bc98816f1a6fafcf53dcb59ee52552902
Shelterin <94851109+shelterin2006@users.noreply.github.com>
2026-03-25 08:51:47 +0700

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.52%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0) return false;
+        if(n == 1) return true;
+        return n % 2 ? false : isPowerOfTwo(n / 2);
+    }
+};
